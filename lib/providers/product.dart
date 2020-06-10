d751b46835d54cedc9df9f03624519a646b6d419
mohammed <m.refaee123580@gmail.com>
2020-06-10 23:55:23 +0400

    auth

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -25,17 +25,18 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
-    final url = 'https://web-host-272719.firebaseio.com/products/$id.json';
+    final url =
+        'https://web-host-272719.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
     try {
-      final response = await http.patch(
+      final response = await http.put(
         url,
-        body: json.encode({
-          'isFavorite': isFavorite,
-        }),
+        body: json.encode(
+          isFavorite,
+        ),
       );
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
